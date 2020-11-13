d95a61d4bb53bd2f9d83116441933ae4d1b25bf6
Mael1couvreur <mael.couvreur@kedgebs.com>
2020-11-13 16:13:36 +0100

    n

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :doses, only: [:destroy]
   root to: 'cocktails#index'
-  resources :cocktails, only: %i[index new create show] do
+  resources :doses, only: [:destroy]
+  resources :cocktails, only: %i[new create show] do
     resources :doses, only: %i[new create]
   end
 end
